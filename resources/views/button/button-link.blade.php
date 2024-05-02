b11e7f28495257cbfd5f2c267fbdd289cb2a5b67
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-02 18:33:29 +0200

    Removed href attribute on disabled link (span)

--- resources/views/button/button-link.blade.php
+++ resources/views/button/button-link.blade.php
@@ -1,5 +1,5 @@
 @if(!$disabled)
     <a href="{{$href}}" {{ $attributes->merge(['class' => $componentClasses]) }}>{{$slot}}</a>
 @else
-    <span href="{{$href}}" {{ $attributes->merge(['class' => $componentClasses]) }}>{{$slot}}</span>
+    <span {{ $attributes->merge(['class' => $componentClasses]) }}>{{$slot}}</span>
 @endif
