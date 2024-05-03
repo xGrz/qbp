0c66c95dba68cc3b7754b62a3bbfcd4cfa6ec435
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-03 19:35:25 +0200

    Added trait WithComponentProps.

---
+++ src/Traits/WithComponentProps.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace xGrz\Qbp\Traits;
+
+trait WithComponentProps
+{
+    public array $componentProps = [];
+
+    protected function getComponentProperties(): array
+    {
+        if (isset($this->componentProperties['classes']) && is_array($this->componentProperties['classes'])) {
+            $this->componentProperties['classes'] = implode(' ', $this->componentProperties['classes']);
+        }
+        return $this->componentProperties;
+    }
+
+    protected function addClass(string $class, string|null $prefix = null): static
+    {
+        $classes = explode(' ', $class);
+        foreach ($classes as $classValue) {
+            $this->componentProperties['classes'][] = $prefix
+                ? $prefix . ':' . $classValue
+                : $classValue;
+        }
+        return $this;
+    }
+
+    protected function applyTextAlign(string|bool $left, string|bool $right, string|bool $center): void
+    {
+        foreach (['left' => $left, 'right' => $right, 'center' => $center] as $align => $value) {
+            if ($value === true && strtolower($value) !== 'false') {
+                $this->addClass("text-$align");
+            }
+        }
+    }
+
+
+    protected function setupClasses(string $defaultClasses = null): void
+    {
+        if (!isset($this->componentProperties['classes'])) {
+            $this->componentProperties['classes'] = $defaultClasses ? [$defaultClasses] : [];
+        }
+    }
+}
